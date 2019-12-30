00016b0ad87150eee93202c340ea5fcec46fd8ff
Joao Goncalves <gmail@joagonca.com>
2019-12-30 11:42:37 +0000

    finished support for multiple gpx files to show combined stats

--- main.go
+++ main.go
@@ -98,13 +98,75 @@ func printSingleStats(gpxFile *gpx.GPX) {
 
 	fmt.Printf("Minimum elevation: %.2fm\n", gpxFile.ElevationBounds().MinElevation)
 	fmt.Printf("Maximum elevation: %.2fm\n", gpxFile.ElevationBounds().MaxElevation)
-	fmt.Printf("Max up gradient: %.2f (%f, %f, %.2fm) - BETA\n", st.maxUpSlope, st.maxUpSlopePoint.Latitude, st.maxUpSlopePoint.Longitude, st.maxUpSlopePoint.Elevation.Value())
 	fmt.Printf("Max down gradient: %.2f (%f, %f, %.2fm) - BETA\n", st.maxDownSlope, st.maxDownSlopePoint.Latitude, st.maxDownSlopePoint.Longitude, st.maxDownSlopePoint.Elevation.Value())
+	fmt.Printf("Max up gradient: %.2f (%f, %f, %.2fm) - BETA\n", st.maxUpSlope, st.maxUpSlopePoint.Latitude, st.maxUpSlopePoint.Longitude, st.maxUpSlopePoint.Elevation.Value())
 
 	fmt.Println("")
 
 	fmt.Printf("Total distance: %.2f km\n", gpxFile.MovingData().MovingDistance/1000)
 	fmt.Printf("Maximum speed: %.2f km/h (%f, %f, %.2fm)\n", st.maxSpeed/10*3.6, st.maxSpeedPoint.Latitude, st.maxSpeedPoint.Longitude, st.maxSpeedPoint.Elevation.Value())
+}
+
+func printCombinedStats(gpxFiles []*gpx.GPX) {
+	var st *stats
+	var movingTime, stoppedTime, minElevation, maxElevation, minGradient, maxGradient, totalDistance, maxSpeed float64
+
+	minElevation = math.MaxFloat64
+	maxElevation = 0
+	minGradient = -100
+	maxGradient = 0
+	maxSpeed = 0
+
+	for _, gpxFile := range gpxFiles {
+		st = getStats(gpxFile)
+
+		movingTime += gpxFile.MovingData().MovingTime
+		stoppedTime += gpxFile.MovingData().StoppedTime
+
+		if minElevation > gpxFile.ElevationBounds().MinElevation {
+			minElevation = gpxFile.ElevationBounds().MinElevation
+		}
+
+		if maxElevation < gpxFile.ElevationBounds().MaxElevation {
+			maxElevation = gpxFile.ElevationBounds().MaxElevation
+		}
+
+		if minGradient > st.maxDownSlope {
+			minGradient = st.maxDownSlope
+		}
+
+		if maxGradient < st.maxUpSlope {
+			maxGradient = st.maxUpSlope
+		}
+
+		totalDistance += gpxFile.MovingData().MovingDistance
+
+		if maxSpeed < st.maxSpeed {
+			maxSpeed = st.maxSpeed
+		}
+	}
+
+	fmt.Println("== GPX combined stats ==")
+	fmt.Println("")
+
+	movingTimeTime := time.Date(0, 0, 0, 0, 0, int(movingTime), 0, time.UTC)
+	stoppedTimeTime := time.Date(0, 0, 0, 0, 0, int(stoppedTime), 0, time.UTC)
+	totalTime := time.Date(0, 0, 0, 0, 0, int(movingTime+stoppedTime), 0, time.UTC)
+	fmt.Printf("Moving time: %02d:%02d:%02d\n", movingTimeTime.Hour(), movingTimeTime.Minute(), movingTimeTime.Second())
+	fmt.Printf("Stopped time: %02d:%02d:%02d\n", stoppedTimeTime.Hour(), stoppedTimeTime.Minute(), stoppedTimeTime.Second())
+	fmt.Printf("Total time: %02d:%02d:%02d\n", totalTime.Hour(), totalTime.Minute(), totalTime.Second())
+
+	fmt.Println("")
+
+	fmt.Printf("Minimum elevation: %.2fm\n", minElevation)
+	fmt.Printf("Maximum elevation: %.2fm\n", maxElevation)
+	fmt.Printf("Max down gradient: %.2f - BETA\n", minGradient)
+	fmt.Printf("Max up gradient: %.2f - BETA\n", maxGradient)
+
+	fmt.Println("")
+
+	fmt.Printf("Total distance: %.2f km\n", totalDistance/1000)
+	fmt.Printf("Maximum speed: %.2f km/h\n", maxSpeed/10*3.6)
 }
 
 func main() {
@@ -131,8 +193,6 @@ func main() {
 			os.Exit(-1)
 		}
 
-		defer file.Close()
-
 		for bytesRead != 0 {
 			bytesRead, err = file.Read(buffer)
 			data = append(data, buffer[:bytesRead]...)
@@ -144,11 +204,17 @@ func main() {
 			os.Exit(-1)
 		}
 
+		file.Close()
+
+		bytesRead = -1
+		buffer = make([]byte, 100)
 		data = data[:0]
 	}
 
 	/* Print stats */
 	if len(gpxFiles) == 1 {
 		printSingleStats(gpxFiles[0])
+	} else {
+		printCombinedStats(gpxFiles)
 	}
 }
